e985d9dea258c2ff84cf2a624e64454c84d5a6b9
madhavi <madhavilatha.008@gmail.com>
2023-04-23 17:00:28 -0500

    payment service

--- payment.sh
+++ payment.sh
@@ -19,7 +19,7 @@ echo -e "\e[36m>>>>>> install dependencies <<<<<<<\e[0m"
 pip3.6 install -r requirements.txt
 
 echo -e "\e[36m>>>>>> setup systemd service <<<<<<<\e[0m"
-cp  /home/roboshop/roboshop---shell/payment.service /etc/systemd/system/payment.service
+cp  /home/centos/roboshop---shell/payment.service /etc/systemd/system/payment.service
 
 echo -e "\e[36m>>>>>> start payment service <<<<<<<\e[0m"
 systemctl daemon-reload

3dfe1b160477dba652231967fe3fadefee6f4ef8
madhavi <madhavilatha.008@gmail.com>
2023-04-23 08:38:55 -0500

    mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -10,6 +10,5 @@ yum install mysql-community-server -y
 echo -e "\e[36m>>>>>> statrt mysql <<<<<<<\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
-
 echo -e "\e[36m>>>>>> reset mysql password <<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
